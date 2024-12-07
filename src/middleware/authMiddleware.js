5d6e502aaf77ddda46da649ccf3902a56a597b27
thona6473 <thona6473@gmail.com>
2024-12-08 00:34:52 +0700

    release V1

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -2,13 +2,11 @@ const jwt = require('jsonwebtoken');
 
 function verifyToken(req, res, next) {
     const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1]; // Extract the token from the "Bearer" scheme
+    const token = authHeader && authHeader.split(' ')[1];
     if (!token) return res.status(401).json({ message: "Authentication required" });
 
     jwt.verify(token, process.env.JWT_KEY, (err, user) => {
         if (err) return res.status(401).json({ message: "Invalid or expired token" });
-
-        // Set the user on the request object
         req.user = user;
         next();
     });
