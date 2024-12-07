eadf1921f4b70e05484f646bff2e78c8fe36256f
thona6473 <thona6473@gmail.com>
2024-12-07 18:36:48 +0700

    fix

--- src/db.js
+++ src/db.js
@@ -1,14 +1,13 @@
 const mongoose = require("mongoose");
-require("dotenv").configure;
+require("dotenv").config(); // Fix: Add the parentheses to call the function
 
-mongoose
-  .connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("MongoDB connected successfully");
-  })
-  .catch(() => {
-    console.log("Failed to connect to MongoDB", err);
-  });
+mongoose.connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log("MongoDB connected successfully");
+    })
+    .catch(() => {
+        console.log("Failed to connect to MongoDB", err);
+    });
