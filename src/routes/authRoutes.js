2f8383c2d600f84dcfaf2808dc0b2678d9a1d296
thona6473 <thona6473@gmail.com>
2024-12-08 02:04:53 +0700

    V@

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,3 +1,27 @@
+// const express = require("express");
+// const router = express.Router();
+// const authController = require("../controller/AuthController");
+// const multer = require("multer");
+// const verifyToken = require('../middleware/authMiddleware');
+//
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, 'upload');
+//   },
+//   filename: function (req, file, cb) {
+//     const filename = file.fieldname + '_' + Date.now() + '-' + Math.round(Math.random() * 1E9) + '.jpg';
+//     req.body.profile_picture_path = filename;
+//     cb(null, filename);
+//   }
+// });
+//
+// const upload = multer({ storage: storage });
+//
+// router.post("/register", upload.single('image'), authController.register);
+// router.post("/login", authController.login);
+// router.get("/checkout", verifyToken, authController.checkAuth);
+//
+// module.exports = router;
 const express = require("express");
 const router = express.Router();
 const authController = require("../controller/AuthController");
@@ -19,6 +43,6 @@ const upload = multer({ storage: storage });
 
 router.post("/register", upload.single('image'), authController.register);
 router.post("/login", authController.login);
-router.get("/checkout", verifyToken, authController.checkAuth);
+router.get("/check-auth",verifyToken,authController.checkAuth);
 
 module.exports = router;
